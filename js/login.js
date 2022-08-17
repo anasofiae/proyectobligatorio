82195fd8efd33aff0f366200547f9d60138af08c
anasofiae <anasofiaechart@gmail.com>
2022-08-16 21:38:44 -0300

    Subo válidación y redirección del formulario del login

---
+++ js/login.js
@@ -0,0 +1,21 @@
+document.getElementById("login-form").addEventListener('submit', function (event) {
+    let emailText = document.getElementById('email').value;
+    let passwordText = document.getElementById('password').value;
+
+    if (emailText.length === 0) {
+        event.preventDefault();
+
+        let emailM = document.getElementById('email-message');
+        emailM.innerHTML = 'Ingresa tu email';
+    }
+
+    if (passwordText.length === 0) {
+        event.preventDefault();
+
+        let passwordM = document.getElementById('password-message');
+        passwordM.innerHTML = 'Ingresa tu contraseña';
+    } else {
+        event.preventDefault();
+        window.location.href = 'index.html';
+    }
+});
