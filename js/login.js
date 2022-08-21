0251361352fe37bb454fa56100c8674c3f9148ff
anasofiae <anasofiaechart@gmail.com>
2022-08-20 23:25:23 -0300

    subo cambios

--- js/login.js
+++ js/login.js
@@ -4,20 +4,14 @@ document.getElementById("login-form").addEventListener('submit', function (event
 
     if (emailText.length === 0) {
         event.preventDefault();
-
         document.getElementById('email').style.borderColor = "red";
-        
-        let emailM = document.getElementById('email-message');
-        emailM.innerHTML = 'Ingresa tu email';
+        document.getElementById('email-message').innerHTML = 'Ingresa tu email'; 
     }
 
     else if (passwordText.length === 0) {
         event.preventDefault();
-
         document.getElementById('password').style.borderColor = "red";
-        
-        let passwordM = document.getElementById('password-message');
-        passwordM.innerHTML = 'Ingresa tu contraseña';
+        document.getElementById('password-message').innerHTML = 'Ingresa tu contraseña';
     } else {
         event.preventDefault();
         window.location.href = 'home.html';
